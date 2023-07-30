66143a1a6f3d88b0cd827058edc610334981bc3d
rhysez <121772484+rhysez@users.noreply.github.com>
2023-07-30 12:17:22 +0100

    add components dir and general react component

---
+++ src/components/General.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function General() {
+    return (
+        <div>
+            <fieldset>
+                <legend>General Information</legend>
+                <form className='cvSection'>
+                    <label htmlFor="name">Full name</label>
+                    <input type="text" id='name' name='name' minLength={4} />
+
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" id='email' name='email' minLength={10} />
+
+                    <label htmlFor="contactNumber">Contact number</label>
+                    <input type="number" id='contactNumber' name='contactNumber' />
+                </form>
+                <button type="submit" id='submit'>Done</button>
+            </fieldset>
+        </div>
+    )
+}
+
